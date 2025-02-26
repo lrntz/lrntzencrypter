90aad615b4785df94060a59a15f7013e7eb781c5
Lorentz <simon@steimler.org>
2025-02-26 23:23:42 +0100

    Enforce minimum codestrength
     * Enforced minimum 2 character codestrength when generating codes to prevent duplicate codes.

--- keygenerate.py
+++ keygenerate.py
@@ -2,8 +2,11 @@ import random
 import string
 import json
 
-#The function that assigns codes to characters
-codestrength = int(input(f"How strong do you want the encryption to be? (Minimum of 2)\n\n"))
+codestrength = 0
+
+while codestrength < 2:
+    #The function that assigns codes to characters
+    codestrength = int(input(f"\nHow strong do you want the encryption to be? (Minimum of 2)\n\n"))
 
 def makechar(codestrength):
     character = random.choice(string.digits)
